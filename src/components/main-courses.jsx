67469fa205f992b4520408b5152470900384f990
Colden111 <muradqenberov31@gmail.com>
2025-12-04 11:05:54 +0400

    commit

--- src/components/main-courses.jsx
+++ src/components/main-courses.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import CourseCard from "./CourseCard";
 import coursesData from "../data/coursesData";
+import "./MainCourses.css";
 
 const MainCourses = () => {
   return (
-    <div style={{ padding: "40px", maxWidth: "1200px", margin: "0 auto" }}>
-      <h1 style={{ textAlign: "center", marginBottom: "10px" }}>
-        Tədris Sahələri
-      </h1>
-      <p style={{ textAlign: "center", marginBottom: "40px", color: "#666" }}>
-        JET Academy-də tədris sahələri müasir əmək bazarının tələblərinə uyğun
-        formalaşdırılıb. Hər bir sahə üzrə proqramlar praktiki bilikləri, real
-        layihələrə və peşəkar müəllif imkanlarına əsaslanır
-      </p>
+    <div className="main-courses">
+      <div className="courses-header">
+        <h1 className="courses-title">Tədris Sahələri</h1>
+        <p className="courses-description">
+          JET Academy-də tədris sahələri müasir əmək bazarının tələblərinə uyğun
+          formalaşdırılıb. Hər bir sahə üzrə proqramlar praktiki bilikləri, real
+          layihələrə və peşəkar müəllif imkanlarına əsaslanır
+        </p>
+      </div>
 
-      <div>
+      <div className="courses-grid">
         {coursesData.map((course) => (
           <CourseCard key={course.id} {...course} />
         ))}
